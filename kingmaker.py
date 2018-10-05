e91c280e439367a878860be1cf161d3c4d179ace
Silvano Cerza <silvanocerza@gmail.com>
2018-10-05 15:37:37 +0200

    Implemented scraper to get all projects comments

---
+++ kingmaker.py
@@ -0,0 +1,82 @@
+import sys
+import os
+import time
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+def get_comments(project_comments_url):
+    # Opens project comments page
+    driver = webdriver.Firefox()
+    driver.get(project_comments_url)
+
+    # Get comments container and buttons to load more comments
+    comments_container = driver.find_element_by_id("react-project-comments")
+    load_reply = comments_container.find_elements_by_class_name("bttn-white")
+    load_comments = comments_container.find_elements_by_class_name("bttn-blue")
+
+    # Keeps loading comments until there are no more to load
+    while len(load_reply) > 0 or len(load_comments) > 0:
+        for button in comments_container.find_elements_by_class_name("bttn-white"):
+            button.click()
+        for button in comments_container.find_elements_by_class_name("bttn-blue"):
+            button.click()
+
+        # Waits loading of new comments
+        time.sleep(2)
+
+        # Gets buttons again
+        load_reply = comments_container.find_elements_by_class_name("bttn-white")
+        load_comments = comments_container.find_elements_by_class_name("bttn-blue")
+
+    comments = []
+
+    for comment in comments_container.find_elements_by_tag_name("li"):
+        # Comments from user that removed their pledge are hidden
+        is_backer = True
+        try:
+            show_comment = comment.find_element_by_link_text("Show the comment.")
+            show_comment.click()
+            is_backer = False
+        except NoSuchElementException:
+            pass
+
+        is_superbacker = False
+        is_collaborator = False
+        is_creator = False
+        try:
+            author_type = comment.find_element_by_xpath(".//span[contains(@class, 'mr1')]")
+            is_superbacker = author_type == "Superbacker"
+            is_collaborator = author_type == "Collaborator"
+            is_creator = author_type == "Creator"
+        except NoSuchElementException:
+            pass
+
+        author = comment.find_element_by_xpath(".//span[contains(@class, 'mr2')]").text
+        text = comment.find_elements_by_tag_name("div")[3].text
+        timestamp = int(comment.find_element_by_tag_name("time").get_attribute("datetime"))
+        comments.append(
+            {
+                "is_backer": is_backer,
+                "is_superbacker": is_superbacker,
+                "is_collaborator": is_collaborator,
+                "is_creator": is_collaborator,
+                "author": author,
+                "text": text,
+                "timestamp": timestamp,
+            }
+        )
+
+    driver.quit()
+    return comments
+
+
+if __name__ == "__main__":
+    os.environ["MOZ_HEADLESS"] = "1"
+    if len(sys.argv) > 1:
+        print(get_comments(sys.argv[1]))
+    else:
+        print("Usage:")
+        print("python kingmaker.py <project_comments_url>")
+
